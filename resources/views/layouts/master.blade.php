e2e6f0023bd6c6803b1b0067874a59e94d455b29
Scot Favorite <scot.favorite@gmail.com>
2019-10-31 10:45:13 -0400

    Change matomo url

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -27,7 +27,7 @@
       _paq.push(['trackPageView']);
       _paq.push(['enableLinkTracking']);
       (function() {
-        var u="//analytics.scotfavorite.net/";
+        var u="//analytics.scotfavorite.com/";
         _paq.push(['setTrackerUrl', u+'piwik.php']);
         _paq.push(['setSiteId', '1']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
